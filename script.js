dfc15696291aff2af1d42cb432329a1f86b89a8d
Adam Nowak <nowak.adam2222@gmail.com>
2023-02-28 00:07:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+{
+    const tasks = [
+        {
+            name: "asdf",
+            done: false,
+        },
+        {
+            name: "ghjk",
+            done: true,
+        },
+        {
+            name: "adim",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                ${task.name}
+                <button class="js-delete">Usuń</button>
+                <button class="js-done">Zrobione!</button>
+            </li>
+            `
+        };
+
+        document.querySelector(".js-list").innerHTML = htmlString;
+
+        removeButton();
+        taskDoneCheck();
+    };
+
+    const taskDoneCheck = () => {
+        const doneButtons = document.querySelectorAll(".js-done");
+
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                tasks[index].done = !tasks[index].done;
+                render();
+            });
+        })
+    };
+
+    const removeButton = () => {
+        const deleteButtons = document.querySelectorAll(".js-delete");
+
+        deleteButtons.forEach((deleteButton, index) => {
+            deleteButton.addEventListener("click", () => {
+                tasks.splice(index, 1);
+                render();
+            });
+        });
+    };
+
+    const addTask = () => {
+
+        const newTask = document.querySelector(".js-button");
+
+        newTask.addEventListener("click", (event) => {
+
+            event.preventDefault();
+
+            const newTaskName = document.querySelector(".js-input").value.trim();
+
+            tasks.push({
+                name: newTaskName,
+                done: false,
+            });
+
+            render();
+        });
+    };
+
+    const init = () => {
+        addTask();
+        render();
+    };
+    init();
+}
